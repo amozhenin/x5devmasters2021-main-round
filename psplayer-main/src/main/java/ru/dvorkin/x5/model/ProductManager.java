25218ee47d5b9066d2a5f36daccd0501c971a95b
Alexey Mozhenin <dvorkin83@gmail.com>
2021-09-09 02:50:49 +0600

    Changing list of products

--- psplayer-main/src/main/java/ru/dvorkin/x5/model/ProductManager.java
+++ psplayer-main/src/main/java/ru/dvorkin/x5/model/ProductManager.java
@@ -21,7 +21,9 @@ public class ProductManager {
     }
 
     public List<Integer> getUsedProductIds() {
-        return List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+        //return List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+        return List.of(16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
+        //return List.of(31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45);
     }
 
     public Integer getQuantityToBuy(int productId, int rackId) {
