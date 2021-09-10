c2e4aee4f0b38aa5e02af5de33b9c81441463c49
Alexey Mozhenin <dvorkin83@gmail.com>
2021-09-10 16:27:00 +0600

    Playing with prices

--- psplayer-main/src/main/java/ru/dvorkin/x5/model/ProductManager.java
+++ psplayer-main/src/main/java/ru/dvorkin/x5/model/ProductManager.java
@@ -69,7 +69,7 @@ public class ProductManager {
     }
 
     public Double getSellPrice(int productId, double stockPrice) {
-        return 1.205 * stockPrice;
+        return 1.207 * stockPrice;
     }
 
     public Integer getProductIdForRack(int rackId) {
