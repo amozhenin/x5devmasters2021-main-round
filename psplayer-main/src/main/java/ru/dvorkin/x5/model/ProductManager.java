5bfef9845ef71ac65e31e9b3983b4541d030c5f1
Alexey Mozhenin <dvorkin83@gmail.com>
2021-09-13 01:39:11 +0600

    Test

--- psplayer-main/src/main/java/ru/dvorkin/x5/model/ProductManager.java
+++ psplayer-main/src/main/java/ru/dvorkin/x5/model/ProductManager.java
@@ -78,7 +78,7 @@ public class ProductManager {
             case 1:
             case 2:
             case 3:
-                return 1.43;
+                return 1.42;
             case 4:
             case 5:
             case 6:
