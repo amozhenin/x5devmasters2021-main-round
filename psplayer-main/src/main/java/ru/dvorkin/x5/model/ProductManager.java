58fe86ccf3bd21277fbaf5aa1f29499133a84e73
Alexey Mozhenin <dvorkin83@gmail.com>
2021-10-16 01:38:10 +0600

    Updating price and default buys

--- psplayer-main/src/main/java/ru/dvorkin/x5/model/ProductManager.java
+++ psplayer-main/src/main/java/ru/dvorkin/x5/model/ProductManager.java
@@ -50,7 +50,7 @@ public class ProductManager {
                     return 0;
                 case 8:
                 case 9:
-                    return 8300;
+                    return 8000;
                     //return 5100;
                 case 10:
                 case 11:
@@ -90,7 +90,7 @@ public class ProductManager {
             case 7:
             case 8:
             case 9:
-                return 1.195;
+                return 1.197;
             case 10:
             case 11:
             case 12:
